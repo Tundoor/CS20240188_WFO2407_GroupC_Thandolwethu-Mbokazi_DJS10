17bac0ff4315b4fdbc61f0ed22be43f333dfbdbd
Thandolwethu <thandwem045@gmail.com>
2024-11-24 01:44:03 +0200

    Added a loading state

--- DJS10/src/App.jsx
+++ DJS10/src/App.jsx
@@ -1,28 +1,40 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-    
-   const [ blogs, setState ] = useState(null)
-   
-    useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-       .then(res => res.json())
-       .then(data => setState(data))
-    } , [])
+
+  const [blogs, setState] = useState(null)
+  const [isloading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Data Fetching Failed")
+        }
+        return res.json()
+      })
+      .then(data => {
+        setState(data);
+        setLoading(false)
+      }
+      )
+  }, [])
 
   return (
-  <div className='blog'>
-    <h1>Posts</h1>
-       {blogs && blogs.map((blog) => {
-         return (
-          <>
-          <h2>{blog.id}. {blog.title}</h2>
-          <p>{blog.body}</p>
-          </>
-         )
-       }) }
-  </div>
-  )
+    isloading ? (
+      <div>Loading...</div> // Show the loading message when isloading is true
+    ) : (
+      <div className='blog'>
+        <h1>Posts</h1>
+        {blogs && blogs.map((blog) => (
+          <div key={blog.id}>
+            <h2>{blog.id}. {blog.title}</h2>
+            <p>{blog.body}</p>
+          </div>
+        ))}
+      </div>
+    )
+  );
 }
 
 export default App
